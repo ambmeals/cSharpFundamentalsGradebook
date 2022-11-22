ed5e4eb1264634e0620406c6ddda127db23b531b
Amber Mills <88246893+ambmeals@users.noreply.github.com>
2022-11-22 11:44:13 -0600

    Add project files.

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Calculate Average Points");
+            book.AddGrade(89.5);
+            book.AddGrade(76.7);
+            book.AddGrade(95.5);
+
+
+            var stats = book.GetStatistics();
+
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The grade average is {stats.Average}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+    }
+}
